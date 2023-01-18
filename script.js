e3092acf72ef5f782bd98b8f8ab9a95186cc9065
karynnemoreira <karynnemoreira@yahoo.com.br>
2023-01-18 10:32:25 -0300

    aula manhã 4

---
+++ script.js
@@ -0,0 +1,67 @@
+//OPERADORES LÓGICOS
+// console.log (true && true) //true
+// console.log (true && false) //false
+// console.log (false && false) //false
+
+// let a = 3 > 2 //true
+// let b = 2 > 3 //false
+
+// if (a == true && b == true){
+//     console.log(`As duas condições são verdadeiras`)
+// } else {
+//     console.log(`As duas condições são diferentes`)
+// }
+
+// " " , '' , `` mesma coisa quando se trata de stringo / texto
+
+
+// let c = 20 > 159 //false, pq 20 é MENOR que 159
+// let d = -2 > -48 //true, pq -2 é MAIOR que -48
+
+// if(c == false && d == true){
+//     console.log("As duas informações são verdadeiras")
+// } else{
+//     console.log("tem algo errado")
+// }
+
+// let grazy = 20
+
+// if (grazy == 19) {
+//     console.log (`Acesso liberado`)
+// } else {
+//     console.log("Tá barrada")
+// }
+
+//console.log (false || true)
+
+//EXERCÍCIO - Criar uma condição que exiba no console se você fizer aniversário em Janeiro OU Dezembro
+
+// let nome = "Jésus"
+// let mes = "Outubro"
+
+// if ( (mes === 'Janeiro') || (mes === 'Dezembro')){
+// console.log (`${nome} faz aniversário em Janeiro ou Dezembro`)
+// } else{
+//     console.log (`${nome} faz aniversário em ${mes}`)
+// }
+//----------------
+
+// let cantora = "Kelly Caroline"
+// let _mes = "Janeiro"
+// let idade = 20
+
+// if((_mes === "Dezembro") || (_mes === "Janeiro")){
+// console.log(`A cantora ${cantora} faz aniversário em ${_mes}`)
+// }else{
+// console.log(`A ${cantora} não faz aniversário em Dezembro ou Janeiro `)
+// }
+
+// console.log(`${idade}`)
+
+///Laços de repetição
+
+// console.log (1)
+// console.log (2)
+// console.log (3)
+// console.log (4)
+// console.log (5)
